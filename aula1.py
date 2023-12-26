1ae2531a46a1b1cb1f28b5c68f3564975b9c9504
orodrig0bandeira <128334279+orodrig0bandeira@users.noreply.github.com>
2023-12-26 15:03:36 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,43 @@
+# print serve para exibir no terminal determinada coisa 
+
+# 
+usuario = input("qual é o seu usuário?")
+senha = input("qual é sua senha?")
+u = "negao"
+s = "123"
+
+if usuario == u and senha == s:
+    print("aceito")
+    n1 = float(input("qual a sua peso?")) 
+    n2 = float(input("qual é o seu altura?"))
+    n3 = (float(n1/(n2*n2)))
+    
+    if n3<18.5:
+        print (f"Qual o valor do seu MC {n1/(n2*n2)}")
+        print("magresa")
+
+    elif n3>=18.5 and n3<=24.9:
+        print (f"Qual o valor do seu MC {n1/(n2*n2)}")
+        print("normal")
+
+    elif n3 >=25.0 and n3<=29.9:
+        print (f"Qual o valor do seu MC {n1/(n2*n2)}")
+        print("sobrepeso")
+        
+    else:
+        print("teste")
+
+    n1 = input("queres ficar comigo ?")
+
+    if n1 == "sim":
+        print("vlw")
+
+    elif n1 == "nao":
+        print("TOMANO CU")
+
+    else:
+        print("vou comer do mesmo jeito")
+
+
+else:
+    print("inválido")
